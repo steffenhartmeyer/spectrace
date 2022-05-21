226facffb01e7ffb72c85edb706936a50555c256
Steffen Hartmeyer <steffen.hartmeyer@epfl.ch>
2022-05-21 19:13:19 +0200

    added function

--- R/spectrace_calibrate_light.R
+++ R/spectrace_calibrate_light.R
@@ -3,8 +3,7 @@
 #' This function calibrates the raw light data of the spectrace sensors based
 #' on the provided calibration data.
 #'
-#' @param lightData Data frame with light data exported from
-#'    \link{\code{spectrace_import_light}}.
+#' @param lightData Data frame with light data.
 #' @param cal_data Data frame with calibration factors. Must consist of the
 #'    following columns: "serial", "lux", "410nm", "435nm", "460nm", "485nm",
 #'    "510nm", "535nm","560nm", "585nm", "610nm", "645nm", "680nm", "705nm",
@@ -30,35 +29,31 @@ spectrace_calibrate_light <- function(lightData, cal_data = NULL) {
     stop("Calibration file columns are not correct!")
   }
 
-  # Stop if more than one serial number in data
-  serial_number <- unique(lightData$serial)
-  if (length(serial_number) > 1) {
-    stop("More than one serial number in the provided data!")
-  }
+  cal_factors = cal_data %>%
+    select(!"760nm") %>%
+    dplyr::rename_at(dplyr::vars(lux, "410nm":"730nm"),
+                     ~paste0("c",.x, "_factor"))
 
-  # Calibration factors spectral data
-  cal_factors <- cal_data %>%
-    dplyr::filter(serial == serial_number) %>%
-    dplyr::select("410nm":"730nm") %>%
-    as.numeric()
+  lightData.cal = lightData %>%
+    dplyr::rename_at(dplyr::vars("410nm":"730nm"),
+                     ~paste0("c",.x)) %>%
+    dplyr::left_join(cal_factors, by = c("serial")) %>%
+    dplyr::mutate(lux = lux / clux_factor,
+                  "410nm" = c410nm / c410nm_factor,
+                  "435nm" = c435nm / c435nm_factor,
+                  "460nm" = c460nm / c460nm_factor,
+                  "485nm" = c485nm / c585nm_factor,
+                  "510nm" = c510nm / c510nm_factor,
+                  "535nm" = c535nm / c535nm_factor,
+                  "560nm" = c560nm / c560nm_factor,
+                  "585nm" = c585nm / c585nm_factor,
+                  "610nm" = c610nm / c610nm_factor,
+                  "645nm" = c645nm / c645nm_factor,
+                  "680nm" = c680nm / c680nm_factor,
+                  "705nm" = c705nm / c705nm_factor,
+                  "730nm" = c730nm / c730nm_factor
+                  ) %>%
+    dplyr::select(!c410nm:c730nm_factor)
 
-  # Calibration factor lux data
-  lux_factor <- cal_data %>%
-    dplyr::filter(serial == serial_number) %>%
-    dplyr::select(lux) %>%
-    as.numeric()
-
-  # Raw spectral output
-  rawData <- lightData %>%
-    dplyr::select("410nm":"730nm")
-
-  # Convert raw spectrace output to irradiance (W/m2)
-  irrData <- rawData / cal_factors[col(rawData)]
-
-  # Return data frame
-  lightData.cal <- lightData %>%
-    dplyr::select(serial:uv) %>%
-    tibble::add_column(irrData) %>%
-    dplyr::mutate(lux = lux / lux_factor)
   return(lightData.cal)
 }
