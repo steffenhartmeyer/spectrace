13012cd2497cfdfc38bc48ae679e1ff9a8220369
steffenhartmeyer <steffen.hartmeyer@epfl.ch>
2023-02-14 17:05:47 +0000

    new fun

---
+++ R/spectrace_from_vector.R
@@ -0,0 +1,29 @@
+#' Unpack vector spectral data into columns
+#'
+#' This function unpacks spectral data stored in a vector into wavelength columns.
+#'
+#' @param lightData Data frame containing the light data. Column with vectors of
+#'    spectral data must be named "spectrum" and with wavelength data must be named
+#'    "wavelength".
+#' @return The data frame with the spectral data with one column per wavelength.
+#' @export
+#'
+#' @examples
+spectrace_from_vector <- function(lightData) {
+
+  # Get wavelengths
+  wavelength <- lightData$wavelength[[1]]
+  col_names <- paste0(wavelength, "nm")
+
+  # Get spectra
+  irr_data <- lightData$spectrum %>%
+    unlist() %>%
+    matrix(nrow = nrow(lightData), byrow = TRUE) %>%
+    data.frame()
+  names(spectrum) <- col_names
+
+  # Add to data frame and return
+  lightData %>%
+    dplyr::select(!spectrum, !wavelength) %>%
+    tibble::add_column(irr_data)
+}
