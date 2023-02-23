da966b65b76e4123ab0984c6fdaa20419b957719
steffenhartmeyer <steffen.hartmeyer@epfl.ch>
2023-02-23 11:55:40 +0100

    new quantities

--- R/spectrace_calculate_quantities.R
+++ R/spectrace_calculate_quantities.R
@@ -9,12 +9,16 @@
 #' @param quantities Quantities to be calculated. Can be any or multiple of:
 #'    ("ALL", "sc", "mc", "lc", "mel", "rod", "scEDI", "mcEDI", "lcEDI",
 #'    "melEDI", "rodEDI", "scELR", "mcELR", "lcELR", "melELR", "rodELR",
-#'    "scDER", "mcDER", "lcDER", "melDER", "rodDER", "ill", "CCT"). If "ALL"
-#'    (the default), all quantities will be calculated and added to the data frame.
+#'    "scDER", "mcDER", "lcDER", "melDER", "rodDER", "cie1924_v2_lux",
+#'    "cie2008_v2_lux", "cie2008_v10_lux", "CCT", "cie1931_x", "cie1931_y",
+#'    "cie1964_x", "cie1964_y"). If "ALL" (the default), all quantities will be
+#'    calculated and added to the data frame.
+#' @param resolution String specifying the resolution of the output
+#'    spectrum. Can be "5nm" (default) or "1nm".
 #' @param interp_method Method for interpolation. Can be "pchip" (smooth
 #'    piecewise hermetic interpolation) or "linear". Defaults to "pchip".
-#' @param keep_spectral_data Logical. Should the spectral irradiance columns be kept?
-#'    Defaults to TRUE.
+#' @param keep_spectral_data Logical. Should the spectral irradiance columns be
+#'    kept? Defaults to TRUE.
 #'
 #' @return Data frame extended with specified quantities.
 #'    If \code{keep_spectral_data} is FALSE then the spectral data columns will be
@@ -22,19 +26,22 @@
 #' @export
 #'
 #' @examples
-spectrace_calculate_quantities <- function(lightData,
-                                           quantities =
-                                             c(
-                                               "ALL",
-                                               "sc", "mc", "lc", "mel", "rod",
-                                               "scEDI", "mcEDI", "lcEDI", "melEDI", "rodEDI",
-                                               "scELR", "mcELR", "lcELR", "melELR", "rodELR",
-                                               "scDER", "mcDER", "lcDER", "melDER", "rodDER",
-                                               "ill", "CCT"
-                                             ),
-                                           resolution = c("5nm", "1nm"),
-                                           interp_method = c("pchip", "linear", "none"),
-                                           keep_spectral_data = TRUE) {
+spectrace_calculate_quantities <- function(
+    lightData,
+    quantities =
+      c(
+        "ALL",
+        "sc", "mc", "lc", "mel", "rod",
+        "scEDI", "mcEDI", "lcEDI", "melEDI", "rodEDI",
+        "scELR", "mcELR", "lcELR", "melELR", "rodELR",
+        "scDER", "mcDER", "lcDER", "melDER", "rodDER",
+        "cie1924_v2_lux", "cie2008_v2_lux", "cie2008_v10_lux",
+        "CCT", "cie1931_x", "cie1931_y","cie1964_x", "cie1964_y"
+      ),
+    resolution = c("5nm", "1nm"),
+    interp_method = c("pchip", "linear", "none"),
+    keep_spectral_data = TRUE) {
+
   # Match arguments
   resolution <- match.arg(resolution)
   interp_method <- match.arg(interp_method)
@@ -65,6 +72,7 @@ spectrace_calculate_quantities <- function(lightData,
       irr_interp <- irr_data %>%
         as.matrix()
       reso.num <- 1
+      wl.out <- wl.1nm
     } else {
       if (interp_method == "none" && !all(wl.out %in% wl.in)) {
         stop("Interpolation method is 'none', but data seems not to be interpolated.")
@@ -85,18 +93,19 @@ spectrace_calculate_quantities <- function(lightData,
     irr_interp[negatives] <- 0
   }
 
-  # Choose matching response functions
-  if (reso.num == 5) {
-    cmf <- cmf.5nm
-    cie_s26e <- cie_s26e.5nm
-  } else {
-    cmf <- cmf.1nm
-    cie_s26e <- cie_s26e.1nm
-  }
+  # Match response functions to resolution
+  v_lambda <- v_lambda_1nm %>% dplyr::filter(wl %in% wl.out)
+  cie_s26e <- cie_s26e_1nm %>% dplyr::filter(wl %in% wl.out)
+  cie_xyz <- cie_xyz_1nm %>% dplyr::filter(wl %in% wl.out)
 
-  # Calculate photopic illuminance
-  K_m <- 683
-  ill <- as.numeric((irr_interp %*% as.numeric(cmf$y)) * K_m * reso.num)
+  # Calculate photopic illuminances
+  K_m <- 683.0015478
+  cie1924_v2_lux <-
+    as.numeric((irr_interp %*% as.numeric(v_lambda$CIE1924_v2)) * K_m * reso.num)
+  cie2008_v2_lux <-
+    as.numeric((irr_interp %*% as.numeric(v_lambda$CIE2008_v2)) * K_m * reso.num)
+  cie2008_v10_lux <-
+    as.numeric((irr_interp %*% as.numeric(v_lambda$CIE2008_v10)) * K_m * reso.num)
 
   # Calculate alpha-opic irradiance and ELR using CIE s26e opsin templates
   scone <- as.numeric((irr_interp %*% as.numeric(cie_s26e$scone)) * reso.num)
@@ -105,32 +114,44 @@ spectrace_calculate_quantities <- function(lightData,
   rod <- as.numeric((irr_interp %*% as.numeric(cie_s26e$rod)) * reso.num)
   mel <- as.numeric((irr_interp %*% as.numeric(cie_s26e$mel)) * reso.num)
   aopic <- cbind(scone, mcone, lcone, mel, rod)
-  elr <- aopic / ill
+  elr <- aopic / cie1924_v2_lux
 
   # Calculate alpha-opic EDI and DER
-  KavD65 <- c(0.8173, 1.4558, 1.6289, 1.4497, 1.3262) / 1000
-  aopic_edi <- aopic / (KavD65)[col(aopic)]
-  der <- elr / (KavD65)[col(elr)]
+  Kav_D65 <- c(0.8173, 1.4558, 1.6289, 1.4497, 1.3262) / 1000
+  aopic_edi <- aopic / (Kav_D65)[col(aopic)]
+  der <- elr / (Kav_D65)[col(elr)]
 
-  # Calculate CIE XYZ using CIE color matching functions
-  x <- (irr_interp %*% as.numeric(cmf$x)) * reso.num
-  y <- (irr_interp %*% as.numeric(cmf$y)) * reso.num
-  z <- (irr_interp %*% as.numeric(cmf$z)) * reso.num
-  cie_x <- x / (x + y + z)
-  cie_y <- y / (x + y + z)
+  # Calculate CIE XYZ using CIE1931 color matching functions
+  CIE1931_x <- (irr_interp %*% as.numeric(cie_xyz$CIE1931_x)) * reso.num
+  CIE1931_y <- (irr_interp %*% as.numeric(cie_xyz$CIE1931_y)) * reso.num
+  CIE1931_z <- (irr_interp %*% as.numeric(cie_xyz$CIE1931_z)) * reso.num
+  CIE1931_xyz <- CIE1931_x + CIE1931_y + CIE1931_z
+  cie1931_x <- CIE1931_x / CIE1931_xyz
+  cie1931_y <- CIE1931_y / CIE1931_xyz
+
+  # Calculate CIE XYZ using CIE1964 color matching functions
+  CIE1964_x <- (irr_interp %*% as.numeric(cie_xyz$CIE1964_x)) * reso.num
+  CIE1964_y <- (irr_interp %*% as.numeric(cie_xyz$CIE1964_y)) * reso.num
+  CIE1964_z <- (irr_interp %*% as.numeric(cie_xyz$CIE1964_z)) * reso.num
+  CIE1964_xyz <- CIE1964_x + CIE1964_y + CIE1964_z
+  cie1964_x <- CIE1964_x / CIE1964_xyz
+  cie1964_y <- CIE1964_y / CIE1964_xyz
 
   # Calculate CCT using McCamy's approximation
-  n <- (cie_x - 0.3320) / (cie_y - 0.1858)
+  n <- (cie1931_x - 0.3320) / (cie1931_y - 0.1858)
   CCT <- -449 * n^3 + 3525 * n^2 - 6823.3 * n + 5520.33
 
   # Combine into data frame
-  cData <- data.frame(cbind(ill, aopic, aopic_edi, elr, der, CCT))
+  cData <- data.frame(aopic, aopic_edi, elr, der,
+                      cie1924_v2_lux, cie2008_v2_lux, cie2008_v10_lux,
+                      cie1931_x, cie1931_y, cie1964_x, cie1964_y, CCT)
   names(cData) <- c(
-    "ill", "sc", "mc", "lc", "mel", "rod",
+    "sc", "mc", "lc", "mel", "rod",
     "scEDI", "mcEDI", "lcEDI", "melEDI", "rodEDI",
     "scELR", "mcELR", "lcELR", "melELR", "rodELR",
     "scDER", "mcDER", "lcDER", "melDER", "rodDER",
-    "CCT"
+    "cie1924_v2_lux", "cie2008_v2_lux", "cie2008_v10_lux",
+    "cie1931_x", "cie1931_y","cie1964_x", "cie1964_y", "CCT"
   )
 
   # Select quantities
